ed319437167ca71f1a7164080bdb0ddbadb7a21f
Sasha <646415@student.inholland.nl>
2022-11-23 11:55:04 +0100

    updated branches

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,2 @@
 <?php
-$name = $_GET['name'];
-$birthday = $_GET['birthday'];
-
-$diff = date_diff(date_create($birthday), date_create($today="now"));
-echo 'Hello ' .$name.', your age is '. $diff->format('%y');
+echo "Hello world";
